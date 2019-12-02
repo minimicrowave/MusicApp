a2c9ef8f2275badb696267883e0c9af19e54f6d6
Serene <unexpecte-d@hotmail.com>
2019-12-02 18:54:37 +0800

    Remove unnecessary logs

--- test/db/rollback.spec.js
+++ test/db/rollback.spec.js
@@ -7,13 +7,11 @@ describe('rollback', () => {
 	it('should rollback after insertion', async () => {
 		const allSongs = await songRepository.findAllSongs();
 		const noOfSongs = allSongs.length;
-		console.log('here', noOfSongs);
 
 		await songRepository.insertSong('Hello', 20, 'hi');
 
 		const allNewSongs = await songRepository.findAllSongs();
 		const noOfNewSongs = allNewSongs.length;
-		console.log('hi', noOfNewSongs);
 
 		expect(noOfSongs).to.equal(noOfNewSongs);
 	});
