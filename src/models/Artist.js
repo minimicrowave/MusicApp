e759e307c88f25b90ffdfe95d72f66285b5aaf14
Serene <unexpecte-d@hotmail.com>
2020-02-04 10:43:22 +0800

    Remove unused methods

--- src/models/Artist.js
+++ src/models/Artist.js
@@ -1,16 +1,6 @@
-const { removeDuplicatesFromArray } = require('../utils');
-
 class Artist {
 	constructor(id, name, country, songList = []) {
 		Object.assign(this, { id, name, country, songList });
-	}
-
-	setSongList(songList) {
-		this.songList = songList;
-	}
-
-	addToSongList(song) {
-		this.songList = removeDuplicatesFromArray([ ...this.songList, song ]);
 	}
 }
 
