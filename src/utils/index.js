a4275bd91ba18cd246d326a60027f7ce5cc1487d
Serene <unexpecte-d@hotmail.com>
2020-03-02 20:49:11 +0800

    Integrate sqlite

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,11 @@
 function removeDuplicatesFromArray(array) {
 	return [ ...new Set(array) ];
 }
+
+function isTestEnv() {
+	return process.env.NODE_ENV === 'test';
+}
+
+module.exports = {
+	isTestEnv
+};
